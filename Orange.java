6cfccd655e3c65217af8752e94b12cd3031cdc92
Luiza <luiza.a.r@mail.ru>
2021-09-28 15:07:43 +0300

    Initial commit.

---
+++ Orange.java
@@ -0,0 +1,14 @@
+package geekbrains;
+
+public class Orange extends Fruit {
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "Апельсины";
+    }
+
+}
